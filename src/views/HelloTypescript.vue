f60c470bc9e51c18013abaa882efcf93e6e1c786
liugoldent <liugoldent@gmail.com>
2020-07-23 23:16:50 +0800

    ts init

---
+++ src/views/HelloTypescript.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <input v-model="ParString">
+    <button @click="fetchData(ParString)">toUpper</button>
+    <h1>{{UpperData}} </h1>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, computed } from '@vue/composition-api'
+
+export default defineComponent({
+  setup () {
+      let ParString:string = 'Hello World';
+
+      const fetchData = function(Par:String): any{
+          console.log(Par);
+          return Par.toUpperCase()
+      };
+
+      let UpperData:string = fetchData(ParString);
+
+      return {
+          ParString,
+          UpperData,
+          fetchData,
+      }
+  }
+});
+</script>
