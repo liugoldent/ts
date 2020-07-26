518181ea391222be3088f245070eaf710ed3eb7e
liugoldent <liugoldent@gmail.com>
2020-07-26 22:05:34 +0800

    0724 finish

--- src/views/HelloTypescript.vue
+++ src/views/HelloTypescript.vue
@@ -1,34 +1,79 @@
 <template>
   <div class="home">
-    <input v-model="ParString">
+    <input v-model="ParString.init">
     <!-- <button @click="fetchData(ParString)">toUpper</button> -->
-    <h1>{{ParString}} </h1>
+    <h1>{{ParString.Upper}} </h1>
+    <h2>Count is: {{ data.count }}, double is: {{ data.double }} </h2>
+    <button @click="increment(data)">Click Me </button>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, computed } from '@vue/composition-api'
+import { defineComponent, computed, reactive, ref } from '@vue/composition-api'
 
 export default defineComponent({
   setup () {
-      let ParString:string = 'Hello World';
+      ///////////////////0717 Athena Question
+      interface IParString {
+        init:string,
+        Upper:string
+      }
+      let ParString:IParString = reactive({
+        init:'Hello World',
+        Upper:computed(()=> ParString.init.toUpperCase())
+      });
 
-      /**
-       * @description 大寫
-       */
-      const fetchData = function(Par:String): string{
-          console.log('Par',Par);
-          return Par.toUpperCase()
-      };
+      ///////////////////0724 Athena Question
+      let a:number = 10
+      let b:string = '10'
+      let c:boolean = true
+      let d:undefined = undefined //null 為任何型別的子型別，故也可以設定為其他型別
+      let e:null = null //null 為任何型別的子型別，故也可以設定為其他型別
+      let f //我認為是any，因為如果是以型別推論來說，當賦值後才會決定型別。但以型別註記而言他是any
+      let g:number[] = [1, 2]
+      let h:any[] = [1, '2']
 
-      let UpperData:string = fetchData(ParString);
+      interface Iobject {
+        x:number,
+        y:string
+      }
+      let i:Iobject = { x: 5, y: '6'}
+
+      /////////////////// Vue3 Used
+      let state: number = 0
+
+      let PlusOne : object= computed({
+        get: () => {console.log(state)},
+        set: val => { state = state - 1 }
+      })
+
+      const increment = function(data:IdataInter): void{
+        data.count++
+        console.log(data)
+      }
+      interface IdataInter {
+        count : number;
+        double: number
+      }
+
+      let data:IdataInter = reactive({
+        count : 2,
+        double:computed(()=>{
+          return data.count*2
+        }),
+        triple:computed(()=>{
+          return data.count^3
+        })
+      })
 
       return {
           //0717
           ParString,
-          fetchData,
-          UpperData,
 
           //0724
+          state,
+          PlusOne,
+          increment,
+          data
       }
   }
 });
